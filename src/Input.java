044a049ba183b3fc4c19d34f8f0ef14a88f53243
Nikhil Khatwani <85192316+nikhilkhatwani@users.noreply.github.com>
2025-01-30 20:47:13 +0000

    Initial commit

--- src/Input.java
+++ src/Input.java
@@ -1,1 +1,32 @@
+import java.util.*;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        String name;
+        int age;
+        int weight;
+        boolean smoker;
+        
+        System.out.print("Enter name: ");
+        name = input.nextLine();
+        
+        System.out.print("Enter age: ");
+        age = input.nextInt();
+
+        System.out.print("Enter weight: ");
+        weight = input.nextInt();
+
+        System.out.print("Do you smoke? Y or N: ");
+        smoker = input.next().equals("Y");
+        
+        System.out.println("Your data");
+        System.out.println("- Name: " + name);
+        System.out.println("- Age: " + age);
+        System.out.println("- Weight: " + weight);
+        System.out.println("- Smoker: " + (smoker ? "Yes" : "No"));
+        
+        input.close();
+    }
+}
